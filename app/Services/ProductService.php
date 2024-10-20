1da0d5168c28949ef9e092a86f2713c9ed08f087
Khaled Hossam <kh-hossam@outlook.com>
2024-10-21 01:06:35 +0300

    adjust product index filters, default to null if filter not exist

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -11,22 +11,25 @@ class ProductService
     {
         // Check if name or price filter is applied, Skip cache and query the database directly
         if (!empty($filters['name']) || !empty($filters['price_min']) || !empty($filters['price_max'])) {
-            return Product::when($filters['name'], function ($query, $name) {
+
+            return Product::when($filters['name'] ?? null, function ($query, $name) {
                 return $query->where('name', 'like', "%$name%");
             })
-            ->when($filters['price_min'], function ($query, $price_min) {
+            ->when($filters['price_min'] ?? null, function ($query, $price_min) {
                 return $query->where('price', '>=', $price_min);
             })
-            ->when($filters['price_max'], function ($query, $price_max) {
+            ->when($filters['price_max'] ?? null, function ($query, $price_max) {
                 return $query->where('price', '<=', $price_max);
             })
+            ->with('category')
+            ->latest()
             ->paginate(config('app.pagination'));
         }
 
-        $cacheKey = 'products_listing_' . $filters['page'] ?? 1;
+        $cacheKey = 'products_listing_' . ($filters['page'] ?? 1);
 
         return Cache::remember($cacheKey, config('app.cache_time'), function() {
-            return Product::with('category')->paginate(config('app.pagination'));
+            return Product::with('category')->latest()->paginate(config('app.pagination'));
         });
     }
 
