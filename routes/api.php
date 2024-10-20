fab9eb77facaecbff4727cf7515c1afdd1d36e20
Khaled Hossam <kh-hossam@outlook.com>
2024-10-20 14:16:31 +0300

    create category feature (FormRequests, Controller and ApiResource)

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +12,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/user', [AuthController::class, 'user'])->middleware('auth:sanctum');
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::apiResource('categories', CategoryController::class);
+});
